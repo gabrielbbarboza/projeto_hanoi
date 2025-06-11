5f9c6966328d6b6c01dba42b063a62bcb2e797d1
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-11 20:33:02 -0300

    Update historico.c 03

--- historico.c
+++ historico.c
@@ -10,3 +10,18 @@ void adicionarHistorico(Historico **lista, const char *nome, int movimentos,
     printf("Erro de memória.\n");
     return;
   }
+  strncpy(novo->nome, nome, sizeof(novo->nome));
+  novo->movimentos = movimentos;
+  novo->numDiscos = numDiscos;
+  strncpy(novo->dataHora, dataHora, sizeof(novo->dataHora));
+  novo->proximo = NULL;
+
+  if (*lista == NULL) {
+    *lista = novo;
+  } else {
+    Historico *atual = *lista;
+    while (atual->proximo != NULL)
+      atual = atual->proximo;
+    atual->proximo = novo;
+  }
+}
