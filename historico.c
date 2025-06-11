2b9f9fab9755d98b2fb76d0c8475e1e375261730
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-10 21:24:20 -0300

    Update historico.c 02

--- historico.c
+++ historico.c
@@ -2,3 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void adicionarHistorico(Historico **lista, const char *nome, int movimentos,
+                        int numDiscos, const char *dataHora) {
+  Historico *novo = malloc(sizeof(Historico));
+  if (!novo) {
+    printf("Erro de memória.\n");
+    return;
+  }
