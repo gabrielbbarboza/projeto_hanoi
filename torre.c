f5d698086f0d40d777b8798c5b75f41ddd15a351
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-10 21:21:46 -0300

    Update torre.c 02

--- torre.c
+++ torre.c
@@ -10,3 +10,11 @@ typedef struct {
   int discos[MAX_DISC];
   int topo;
 } Torre;
+
+void inicializarTorres(Torre torres[3], int numDiscos) {
+  for (int i = 0; i < 3; i++)
+    torres[i].topo = -1;
+
+  for (int i = numDiscos; i >= 1; i--)
+    torres[0].discos[++torres[0].topo] = i;
+}
