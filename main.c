d523b68ac286b6ac42636846661db9441b49c113
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-11 20:54:38 -0300

    Update main.c 01

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
+#include "historico.h"
+#include "torre.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Aproveitei a função lerInt que já tem na torre.c pra manter tudo consistente (declarei como extern)
+extern int lerInt(const char *prompt, int min, int max, int cancelarPartida);
+
+int main() {
+  Historico *lista = NULL;
+
+  carregarDoArquivo(&lista, "historico.txt");
+
+  int opcao;
