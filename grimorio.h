ac35aad78e01fd6bbb5676aff6b4db435ed5b916
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-15 19:24:21 -0300

    Criação de grimorio.h
    Precisei adicionar um novo arquivo onde possui todas as funções utilitárias genéricas que podem ser usadas por qualquer parte do programa."grimorio.h" e "grimorio.c".
    
    Isso vai evitar duplicação de código e agrupa funções que não pertencem diretamente a uma funcionalidade principal.

--- grimorio.h
+++ grimorio.h
@@ -1,1 +1,14 @@
+#ifndef GRIMORIO_H 
+#define GRIMORIO_H
 
+#include <stdio.h>
+
+void limparCanalComunicacao();
+
+int decifrarNumero(const char *prompt, int min, int max, int podeDesistir);
+
+int obterTextoDoAventureiro(char *buffer, int tamanhoBuffer, const char *prompt);
+
+void obterDataHoraAtual(char *buffer);
+
+#endif 
