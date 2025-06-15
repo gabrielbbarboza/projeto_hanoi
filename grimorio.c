506adae177fb914669f1f96f096660cefebd2a6c
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-15 16:26:05 -0300

    Criação de grimorio.c
    Precisei adicionar um novo arquivo onde possui todas as funções genéricas que podem ser usadas por qualquer parte do programa."grimorio.h" e "grimorio.c".
    
    Isso vai evitar duplicação de código e agrupa funções que não pertencem diretamente a uma funcionalidade principal.

---
+++ grimorio.c
@@ -0,0 +1,67 @@
+#include "grimorio.h" 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <time.h> 
+
+void limparCanalComunicacao() {
+  int c;
+ 
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
+}
+
+int decifrarNumero(const char *prompt, int min, int max, int podeDesistir) {
+  int numero;
+  char entrada[256]; 
+  while (1) { 
+    printf("%s", prompt); 
+    if (fgets(entrada, sizeof(entrada), stdin) == NULL) {
+      printf("Erro na leitura. Tente novamente.\n");
+      continue;
+    }
+    entrada[strcspn(entrada, "\n")] = '\0';
+
+    if (sscanf(entrada, "%d", &numero) ==
+        1) { 
+      if (podeDesistir && numero == 0) {
+        return 0; 
+      }
+      if (numero >= min && numero <= max) {
+        return numero; 
+      } else {
+        printf(
+            "Aventureiro, o número deve ser entre %d e %d. Tente novamente.\n",
+            min, max);
+      }
+    } else {
+      printf(
+          "Entrada inválida. Por favor, digite um número. Tente novamente.\n");
+    }
+  }
+}
+
+int obterTextoDoAventureiro(char *buffer, int tamanhoBuffer,
+                            const char *prompt) {
+  printf("%s", prompt);
+  if (!fgets(buffer, tamanhoBuffer, stdin)) {
+    return 0; 
+  }
+
+
+  buffer[strcspn(buffer, "\n")] = '\0';
+
+  return 1;
+}
+
+void obterDataHoraAtual(char *buffer) {
+  time_t t = time(NULL); 
+  struct tm tm = *localtime(&t); 
+  snprintf(buffer, 30, "%04d-%02d-%02d %02d:%02d:%02d",
+           tm.tm_year + 1900, 
+           tm.tm_mon + 1,    
+           tm.tm_mday,  
+           tm.tm_hour,  
+           tm.tm_min,
+           tm.tm_sec); 
+}
