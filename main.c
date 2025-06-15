9525dc3e2d2b603b1cb7f43f6d215415060cbe8e
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-15 15:54:22 -0300

    Atualização de main.c
    Precisei adicionar um novo arquivo onde possui todas as funções genéricas que podem ser usadas por qualquer parte do programa."grimorio.h" e "grimorio.c".
    
    Isso vai evitar duplicação de código e agrupa funções que não pertencem diretamente a uma funcionalidade principal.

--- main.c
+++ main.c
@@ -1,47 +1,44 @@
-
-#include "cronicas.h"
+#include "cronicas.h"       
 #include "desafio_enigmatico.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "grimorio.h" 
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main() {
   Cronica *pergaminhoCronicas =
       NULL; 
 
   carregarCronicasDoArquivo(&pergaminhoCronicas,
-                            "cronicas.txt"); 
+                            "cronicas.txt"); /
 
   int escolhaAventureiro;
 
-  while (1) {
-    printf("\n=== O Portal do Guardião ===\n");
+  while (1) { 
+    printf("\n=== O Salão das Três Colunas ===\n");
     printf("1. Enfrentar o Desafio Enigmático (Jogar)\n");
-    printf("2. Consultar as Crônicas Antigas (Ver histórico)\n");
+    printf("2. Consultar as Crônicas Antigas\n");
     printf("3. Buscar uma Lenda Específica (Buscar por nome ou data)\n");
     printf("4. Encerrar a Jornada (Sair)\n");
-    printf("Escolha uma opção"); 
-                                 
-    escolhaAventureiro = decifrarNumero("", 1, 4, 0);
+
+ 
+    escolhaAventureiro = decifrarNumero("Escolha uma opção: ", 1, 4, 0);
 
     switch (escolhaAventureiro) {
     case 1:
-
+     
       iniciarDesafio(&pergaminhoCronicas);
       break;
     case 2:
       exibirCronicas(pergaminhoCronicas);
       break;
-    case 3: { 
+    case 3: {   
       char termoBusca[50]; 
-      printf("Qual lenda buscas (nome do aventureiro ou data da jornada)?: ");
 
-      limparCanalComunicacao();
-      if (fgets(termoBusca, sizeof(termoBusca), stdin)) {
-        termoBusca[strcspn(termoBusca, "\n")] =
-            '\0'; 
+      if (obterTextoDoAventureiro(termoBusca, sizeof(termoBusca),
+                                  "Qual lenda buscas (nome do aventureiro ou "
+                                  "data da jornada)?: ")) {
         buscarCronicasPorNomeOuData(pergaminhoCronicas,
-                                    termoBusca);
+                                    termoBusca); 
       } else {
         printf("Não conseguimos ouvir sua busca, aventureiro.\n");
       }
@@ -52,14 +49,14 @@ int main() {
           "O Guardião fecha o Portal. Até a próxima jornada, aventureiro!\n");
 
       liberarCronicas(pergaminhoCronicas);
-      return 0; 
+      return 0;
     default:
-
+   
       printf(
           "Aventureiro, essa escolha não existe no Portal. Tente novamente.\n");
       break;
     }
   }
 
-  return 0;
+  return 0; 
 }
