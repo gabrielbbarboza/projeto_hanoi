27b4646740ae4682f9db870cece8b340f67e8360
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-15 13:58:08 -0300

    renomear historico.h para cronicas.h
    Trocar 'histórico' por 'crônicas' foi uma decisão para dar mais vida ao jogo. Não muda como o código funciona, mas sim como você, o jogador, sente que está vivendo uma lenda, e não apenas vendo um registro

---
+++ cronicas.h
@@ -0,0 +1,30 @@
+#ifndef CRONICAS_H
+#define CRONICAS_H
+
+typedef struct Cronica {
+  char nome[50];           
+  int movimentos;         
+  int numDiscos;          
+  char dataHora[30];       
+  struct Cronica *proximo; 
+                     
+} Cronica;
+
+
+void adicionarCronica(Cronica **lista, const char *nome, int movimentos,
+                      int numDiscos, const char *dataHora);
+
+void exibirCronicas(Cronica *lista);
+
+void salvarCronicasEmArquivo(Cronica *lista, const char *nomeArquivo);
+
+
+void carregarCronicasDoArquivo(Cronica **lista, const char *nomeArquivo);
+
+void liberarCronicas(Cronica *lista);
+
+void buscarCronicasPorNomeOuData(Cronica *lista, const char *termo);
+
+void obterDataHoraAtual(char *buffer);
+
+#endif
