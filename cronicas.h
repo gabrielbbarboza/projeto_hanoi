964fa53c4cda2200b4a39ff40050ed0fa7929f54
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-16 20:40:26 -0300

    Atualição de cronicas.h
    Adicionando comentários para melhor entendimento do código.

--- cronicas.h
+++ cronicas.h
@@ -2,16 +2,20 @@
 #define CRONICAS_H
 
 typedef struct Cronica {
-  char nome[50];   
-  int movimentos;     
-  int numDiscos;           
-  char dataHora[30];       
-  struct Cronica *proximo;
+  char nome[50];                   // Nome do aventureiro (jogador).
+  int movimentos;                // Número de movimentos realizados para resolver o desafio.
+  int numDiscos;               // Número de discos usados no desafio.
+  char dataHora[30];             // Data e hora de conclusão do desafio.
+  struct Cronica *proximo;         // Ponteiro para o próximo nó na lista encadeada.
+
+// Protótipos das funções para manipulação das crônicas (histórico de partidas).
+// Estas funções são as "ações" que podem ser realizadas com as crônicas.
                          
 } Cronica;
 
 void adicionarCronica(Cronica **lista, const char *nome, int movimentos,
                       int numDiscos, const char *dataHora);
+
 void exibirCronicas(Cronica *lista);
 
 void salvarCronicasEmArquivo(Cronica *lista, const char *nomeArquivo);
