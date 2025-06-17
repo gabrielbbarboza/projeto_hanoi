bcb48468638aa450435d5b7e9e46880c8d1256cf
Gabriel Barbosa <gabriel.bbarbosa@a.ucb.br>
2025-06-16 21:47:39 -0300

    Atualização de desafio_enigmatico.h
    Adicionei alguns comentários para expandir o conhecimento sobre o código.

--- desafio_enigmatico.h
+++ desafio_enigmatico.h
@@ -4,6 +4,9 @@
 #include "cronicas.h" 
 #include "grimorio.h" 
 
+// Protótipo da função principal para iniciar o desafio enigmático (Torre de Hanói).
+// Ela recebe a lista de crônicas para poder adicionar um novo registro ao final da partida.
+
 void iniciarDesafio(Cronica **listaCronicas);
 
-#endif // DESAFIO_ENIGMATICO_H
+#endif
